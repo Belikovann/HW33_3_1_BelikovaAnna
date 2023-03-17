1457a8b13f86b4e250b0389c91b1764a08672aac
@Annabel_ <hydrant94@gmail.com>
2023-03-17 22:27:39 +0400

    Initial Commit

---
+++ HW33_3_1_BelikovaAnna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW33_3_1_BelikovaAnna
+//
+//  Created by Аня Беликова on 17.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
